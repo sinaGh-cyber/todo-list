9041171dfe94e59e6377d071845cf9114eb6cd21
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-01-13 18:08:31 +0330

    fomes ui ready

--- src/pages/add-product.jsx
+++ src/pages/add-product.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import  Classes  from './addProduct.module.css';
+import Classes from './addProduct.module.css';
 
 const AddProduct = () => {
   const {
@@ -15,8 +15,8 @@ const AddProduct = () => {
 
   return (
     <form onSubmit={handleSubmit(submitter)}>
-      <div className={(Classes.myInput, Classes.title)}>
-        <label htmlFor="title">Title</label>
+      <div id={Classes.title}>
+        <label htmlFor="title">Title:</label>
         <input
           {...register('title', {
             required: "you haven't entered title",
@@ -27,41 +27,49 @@ const AddProduct = () => {
         <p className="error"></p>
       </div>
 
-      <div className={(Classes.myInput, Classes.priority)}>
-        <label className="priority">priority</label>
-        <input
-          {...register('priority', {
-            required: "you haven't select a priority",
-          })}
-          type="radio"
-          value="green"
-        />
-        <input
-          {...register('priority', {
-            required: "you haven't select a priority",
-          })}
-          type="radio"
-          value="yellow"
-        />
-        <input
-          {...register('priority', {
-            required: "you haven't select a priority",
-          })}
-          type="radio"
-          value="red"
-        />
+      <div id={Classes.priority}>
+        <label className="priority">priority:</label>
+        <div>
+          <input
+            name="priority"
+            {...register('priority', {
+              required: "you haven't select a priority",
+            })}
+            type="radio"
+            value="1"
+          />
+          <input
+            name="priority"
+            {...register('priority', {
+              required: "you haven't select a priority",
+            })}
+            type="radio"
+            value="2"
+          />
+          <input
+            name="priority"
+            {...register('priority', {
+              required: "you haven't select a priority",
+            })}
+            type="radio"
+            value="3"
+          />
+        </div>
       </div>
 
-      <div className={(Classes.myInput, Classes.description)}>
-        <label htmlFor="description">description</label>
+      <div id={Classes.description}>
+        <label htmlFor="description">description:</label>
         <textarea
+          rows="10"
           id="description"
           {...register('description', {
             max: { value: 'max carechter 1200' },
           })}
         />
       </div>
-      <input type="submit" />
+      <div>
+        <input type="submit" />
+      </div>
     </form>
   );
 };
