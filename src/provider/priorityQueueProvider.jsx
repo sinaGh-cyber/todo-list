fdf0fac11e27cd1467424b30197a980523a0ad89
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-01-16 12:41:39 +0330

    priorityQueue provider almost ready

--- src/provider/priorityQueueProvider.jsx
+++ src/provider/priorityQueueProvider.jsx
@@ -5,13 +5,21 @@ import { createContext } from 'react/cjs/react.development';
 const PriorityQueueContext = createContext(undefined);
 
 const PriorityQueueProvider = ({ children }) => {
+  const [priorityQueue, setPriorityQueue] = useState(new PriorityQueue());
 
+  if (localStorage.getItem('myPriorityQueue')) {
+    priorityQueue.values = localStorage.getItem('myPriorityQueue').json.parse();
+  }
 
-    const [priorityQueue, setPriorityQueue] = useState( new PriorityQueue() )
-
-
+  const updatePriorityQueue = () => {
+    const stringifyPriorityQueue = priorityQueue.values.json.stringify();
+    localStorage.setItem('myPriorityQueue', stringifyPriorityQueue);
+    setPriorityQueue({ ...priorityQueue });
+  };
   return (
-    <PriorityQueueContext.Provider value={{ }}>
+    <PriorityQueueContext.Provider
+      value={{ priorityQueue, updatePriorityQueue }}
+    >
       {children}
     </PriorityQueueContext.Provider>
   );
