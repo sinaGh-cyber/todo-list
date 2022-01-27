0c9991892449b3bba4a3131fb56e1a18cbce7c9c
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-01-27 12:45:37 +0330

    home ui ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
-import Classes from './Home.module.css'
+import Classes from './Home.module.css';
+import { usePriorityQueue} from '../provider/priorityQueueProvider'
+import { useAlert } from '../provider/alertProvider';
 
 const Home = () => {
+
+  const { priorityQueue, updatePriorityQueue } = usePriorityQueue();
+  const { isAlert, setIsAlert } = useAlert();
+
+  let priorityClass;
+  const priorityCode = Number((priorityQueue.values[0].priority + '')[0]);
+
+  if (priorityCode === 3) {
+    priorityClass = Classes.green;
+  } else if (priorityCode === 2) {
+    priorityClass = Classes.yellow;
+  } else if (priorityCode === 1) {
+    priorityClass = Classes.red;
+  } else {
+    priorityClass = Classes.gray;
+  }
+
   return (
-    <section className={Classes.card}>
-      <button className={Classes.do}></button>
+    <section className={`${Classes.card} ${priorityClass}`}>
+      <button className={Classes.delete}>
+        <i className={Classes.icon}></i>
+      </button>
 
       <article className={Classes.content}>
         <h1 className={Classes.title}></h1>
         <div className={Classes.description}></div>
       </article>
 
-      <button className={Classes.delete}></button>
+      <button className={Classes.do}>
+        <i className={Classes.icon}></i>
+      </button>
     </section>
   );
 };
